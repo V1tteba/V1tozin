cac8384d40aa2c4786e52c2c2109f90c2130337c
V1tteba <gabrielguerra3366@gmail.com>
2026-02-04 23:15:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const porta = document.getElementById("porta");
 const cama = document.getElementById("cama");
 const janela = document.getElementById("janela");
 const espelho = document.getElementById("espelho");
+
+// Redireciona para o site externo (substituindo o antigo index)
 porta.addEventListener("click", () => {
-    window.location.href = "index.html"; 
+    window.location.href = "https://v1tteba.neocities.org"; 
 });
+
 cama.addEventListener("click", () => {
     window.location.href = "bed.html"; 
 });
+
 espelho.addEventListener("click", () => {
     window.location.href = "itsme.html"; 
 });
+
 janela.addEventListener("click", () => {
     window.location.href = "window.html"; 
 });
